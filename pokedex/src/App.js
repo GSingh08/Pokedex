aef4736d9e8afaa478ff696dc5022171ce0761d8
Gurjot Singh <gurjotsingh@Gurjots-MacBook-Pro.local>
2018-06-04 10:28:31 -0400

    created fetch for pokemon

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -5,6 +5,28 @@ import PokeList from './PokeList';
 import PokeProfile from './PokeProfile';
 
 class App extends Component {
+  constructor(){
+    super(props)
+    this.state = {
+      pokeList: null,
+      urlList:null,
+      pokeListLoaded:false,
+      urlListLoaded:false
+    }
+  }
+//This calls to pokemon api and gets the 151 pokemon.
+componentDidMount(){
+  fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+  .then(res => res.json())
+  .then(data => {
+    this.state({
+      pokeList: data.results,
+      pokeDataLoaded:true
+    })
+  })
+}
+
+
   render() {
     return
   }
